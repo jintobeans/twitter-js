9528a458e216ae87f12951140060a64b521c2fa7
jt wang <jtjtjtjtw@gmail.com>
2017-11-07 10:06:39 -0600

    updated files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const volleyball = require('./volleyball');
+const nunjucks = require('nunjucks');
 
 app.listen(3000, function(){
     console.log('server listening');
@@ -13,11 +14,38 @@ app.use('/',function(req, res, next){
     next();
 })
 
-
 app.get('/', function(req, res){
-    res.send('hello world!!!');
+    res.send( 'Hello world');
 });
 
 app.get('/news', function(req, res){
     res.send('whatsup');
 });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+var locals = {
+    title: 'Toozday',
+    people: [
+        { name: 'ambil'},
+        { name: 'hairysmelly'},
+        { name: 'jintobeans'}
+    ]
+};
+
+nunjucks.render('index.html', function(err, output ){
+    if (err) throw err;
+    console.log(output);
+});
+
+app.get('/template', function(req, res){
+    res.render('index.html', locals);
+})
+
+
+
+
+
+
